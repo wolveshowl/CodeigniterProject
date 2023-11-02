8c397d3a5fa33166901a21c6f692a7ceb50f81fd
cnk <dipper25@naver.com>
2023-11-02 18:19:02 +0900

    FEAT: API 라우팅 설정

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,15 +1,21 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
+use App\Controllers\Auth;
 
 /**
  * @var RouteCollection $routes
  */
-// $routes->get('/', 'Home::index');
-$routes->get('/member', 'MemberController::index');
-$routes->get('/', 'MemberController::phpInfo');
-$routes->post('/login', 'LoginController::index');
-$routes->post('/signup', 'RegisterController::index');
-$routes->post('/decode', 'LoginController::decode');
-$routes->post('/update', 'LoginController::accessTokenUpdate');
+
+$routes->group('/', ['filter' => 'Authentication'], function ($routes) {
+    // 라우트 설정 추가
+    $routes->post('api/decode', 'Auth::decode');    
+    $routes->post('api/update', 'Auth::accessTokenUpdate');              
+});
+
+// 토큰이 필요하지 않은 엔드포인트 엑세스 허용
+$routes->get('/info', 'Member::phpInfo');
+$routes->post('/login', 'Auth::login'); 
+$routes->post('/signup', 'Register::index');
+
 
