5f583be1e57a0ed240160c197af83e33c2fe1352
cnk <dipper25@naver.com>
2023-11-03 17:39:04 +0900

    FEAT: CRUD 가독성 주석 추가

--- app/Models/MemberModel.php
+++ app/Models/MemberModel.php
@@ -16,24 +16,42 @@ class MemberModel extends Model {
     protected $useSoftDeletes       = false;    
     protected $allowedFields        = ['access_token', 'refresh_token'];
 
+
+    // SELECT -----------------------------------------------
+
     public function getMemberInfo() {        
         return $this->findAll();
     }
 
     public function getMemberOne($id) {
-        return $this->db->table("member")->where("mem_id", $id)->get()->getRow();    
+        return $this->db->table("member")->where("mem_id", $id)->get()->getRow();            
     }
+    
+    public function selectAccessTokenByRefreshToken($data) {
+        return $this->db->table("member")->where("refresh_token", $data)->get()->getRow();
+    }
+
+    // ------------------------------------------------------
+
+
+
+    // INSERT -----------------------------------------------
 
     public function insertMember($data) {
         return $this->insert($data);
     }
-    
+
+    // ------------------------------------------------------
+
+
+
+    // UPDATE -----------------------------------------------
+
     public function updateAccessAndRefreshToken($data) {
         $this->db->table("member")->where("mem_no", $data['mem_no'])->update($data);        
     }
 
-    public function selectAccessTokenByRefreshToken($data) {
-        return $this->db->table("member")->where("refresh_token", $data)->get()->getRow();
-    }
+    // ------------------------------------------------------
+        
 }
 ?>
