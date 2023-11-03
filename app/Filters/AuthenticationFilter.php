fb51d044981b37f4fedeaa0cd5b195c987a54741
cnk <dipper25@naver.com>
2023-11-03 17:37:20 +0900

    FIX: 인증 필터 클래스 수정

--- app/Filters/AuthenticationFilter.php
+++ app/Filters/AuthenticationFilter.php
@@ -10,6 +10,8 @@ use Config\Services;
 use Firebase\JWT\ExpiredException;
 use CodeIgniter\HTTP\URI;
 
+use App\Models\MemberModel;
+
 use JWT_helper;
 helper('jwt_helper');
 helper('cookie');
@@ -20,20 +22,21 @@ class AuthenticationFilter implements FilterInterface {
         
         $uri = new URI(current_url());
         $jwtHelper = new JWT_helper();
+        $memberModel = new MemberModel();
                     
     
         // 헤더에 토큰이 없다면 로그인 페이지로 이동
-        // $authHeader = $request->getHeaderLine('Authorization');
-        $token = get_cookie('accessToken');
-
-        $_SERVER['HTTP_AUTHORIZATION'] = $token;
+        $authHeader = $request->getHeaderLine('Authorization');
         
-        if (empty($_SERVER['HTTP_AUTHORIZATION'])) {                       
+        if (empty($authHeader)) {                                   
             return site_url('info');               
-        }        
-    
+        } 
+        $token = get_cookie('accessToken');
+        $_SERVER['HTTP_AUTHORIZATION'] = $token;
+       
         try {
-            $decoded = $jwtHelper->jwtDecodeData($_SERVER['HTTP_AUTHORIZATION']);            
+            $decoded = $jwtHelper->jwtDecodeData($_SERVER['HTTP_AUTHORIZATION']);
+            $findMember = $memberModel->getMemberOne($decoded->id);             
         } catch (ExpiredException $e) {
             return Services::response()->setStatusCode(401, '토큰 만료');
         } catch (\Exception $e) {
